740556f19b272c126b24e274d6884689fbeccbff
Amarachukwu Onuoha <amarachukwuonuohabars007@gmail.com>
2026-06-09 23:11:04 +0100

    type issues

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -40,7 +40,7 @@ export const authOptions: NextAuthOptions = {
     async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
-        token.role = user.role;
+        token.role = (user as any).role;
       }
       return token;
     },
